639e9244f328a4b49d17437d25ba9050db43a118
Tien Nguyen <tnguyen0393@gmail.com>
2020-06-10 09:56:57 -0500

    included searchbox to filter and return an array of robots

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
 import Cardlist from "./components/cardlist/cardlist.component";
+import SearchBox from "./components/searchbox/searchbox.component";
 import "tachyons";
 
 import { robots } from "./robots";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+
+  onSearch = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
   render() {
-    return <Cardlist robots={robots} />;
+    const filteredRobot = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    console.log(filteredRobot);
+    return (
+      <div className="tc">
+        <h1>Robo Friends!</h1>
+        <SearchBox searchChange={this.onSearch} />
+        <Cardlist robots={filteredRobot} />
+      </div>
+    );
   }
 }
 
